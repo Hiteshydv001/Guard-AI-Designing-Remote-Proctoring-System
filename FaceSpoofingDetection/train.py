89bc5e30eace357e952d97ba15fc995257c9dd00
Nikhil <nikhilbalamurugan@gmail.com>
2025-02-02 11:05:05 +0530

    Push only FaceSpoofingDetection folder

--- FaceSpoofingDetection/train.py
+++ FaceSpoofingDetection/train.py
@@ -1,10 +1,8 @@
 from model.face_spoofing_model import create_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Create model
 model = create_model()
 
-# Image data generators for loading and augmenting images
 train_datagen = ImageDataGenerator(
     rescale=1./255, 
     rotation_range=20, 
@@ -29,10 +27,8 @@ validation_generator = validation_datagen.flow_from_directory(
     class_mode='binary'
 )
 
-# Train the model
 model.fit(train_generator, validation_data=validation_generator, epochs=10)
 
-# Save the trained model
 model.save('model/face_spoofing_model.h5')
 
 print("Model training complete! Saved as 'model/face_spoofing_model.h5'.")
